ac9036884efec5ffe436adfa9caa6802eb0a8ef0
Hikmahx <hikmayousuph@gmail.com>
2023-08-22 19:31:27 +0100

    TagsInput

---
+++ frontend/components/TagsInput.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TagsInput = () => {
+  return (
+    <View>
+      <Text>TagsInput</Text>
+    </View>
+  )
+}
+
+export default TagsInput
