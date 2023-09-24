5b255bc3aba8bd80db7b396ebe6f03b212024ead
Hikmahx <hikmayousuph@gmail.com>
2023-09-24 20:23:13 +0100

    get todos query

--- frontend/GraphQL/Queries/todoQueries.ts
+++ frontend/GraphQL/Queries/todoQueries.ts
@@ -1,1 +1,15 @@
+import { gql } from "@apollo/client";
 
+const GET_TODOS = gql`
+  query getTodos {
+    todos {
+      id
+      todo
+      completed
+      tags
+    }
+  }
+`;
+
+
+export { GET_TODOS };
--- frontend/components/Todos.tsx
+++ frontend/components/Todos.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext } from "react";
-import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity, Button } from "react-native";
 import TodoItem from "../components/TodoItem";
 import Loading from "./Loading";
 import tw from "../lib/tailwind";
@@ -7,10 +7,20 @@ import { useSelector, useDispatch } from "react-redux";
 import { AppDispatch, RootState } from "../redux/store";
 import Bottom from "./Bottom";
 import { todos } from "../todos.json";
+import { GET_TODOS } from "../GraphQL/Queries/todoQueries";
+import { useQuery } from "@apollo/client";
 
 const Todos = () => {
-  const { darkMode, error, errMessage, total, filterTodos, loading } =
-    useSelector((state: RootState) => state.todo);
+  const { loading, error, data, refetch } = useQuery(GET_TODOS);
+
+  const {
+    darkMode,
+    // error,
+    errMessage,
+    total,
+    filterTodos,
+    //  loading
+  } = useSelector((state: RootState) => state.todo);
   const dispatch = useDispatch<AppDispatch>();
 
   interface TodoItem {
@@ -19,11 +29,28 @@ const Todos = () => {
     completed: boolean;
   }
 
+  interface Data {
+    id: number;
+    todo: string;
+    completed: boolean;
+  }
+  [];
+
   if (error) {
+    console.log(error);
+
     return (
       <View style={tw`mt-16`}>
-        <Text style={tw`text-center`}>{errMessage}</Text>
-        <Text style={tw`text-center`}>Please reload page</Text>
+        <Text style={tw`text-center`}>{error.message}</Text>
+        <Button title="Reload Data" onPress={() => refetch()} />
+      </View>
+    );
+  }
+
+  if ( loading ) {
+    return (
+      <View style={tw`flex justify-center mx-auto mt-6`}>
+        <Loading />
       </View>
     );
   }
@@ -35,14 +62,14 @@ const Todos = () => {
           !darkMode ? "bg-white" : "bg-very-dark-desaturated-blue"
         }`}
       >
-        {loading && (
+        {/* {loading && (
           <View style={tw`flex justify-center mx-auto mt-6`}>
             <Loading />
           </View>
-        )}
+        )} */}
 
         <View style={tw`max-w-34rem mx-auto w-full`}>
-          {todos.map((item) => (
+          {data.todos.map((item: Data) => (
             <TodoItem key={item.id} item={item} />
           ))}
 
@@ -73,7 +100,7 @@ const Todos = () => {
             </TouchableOpacity>
           </View>
           <View
-            style={tw` flex flex-col items-center relative   shadow-xl rounded`}
+            style={tw` flex flex-col items-center relative shadow-xl rounded`}
           ></View>
         </View>
       </View>
