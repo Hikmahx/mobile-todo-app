0a18f8d948a8b7f40ba7e23cfb03f7d3459745af
Hikmahx <hikmayousuph@gmail.com>
2023-09-26 20:35:56 +0100

    uri updated

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -13,7 +13,7 @@ import MyStack from "./navigation/AppNavigator";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://localhost:5000/graphql",
+  uri: "http://localhost:5000/graphql",
   cache: new InMemoryCache(),
 });
 
