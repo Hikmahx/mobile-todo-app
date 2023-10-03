d03328019e0fe6b2c4a5fc3296df2c728abef0c0
Hikmahx <hikmayousuph@gmail.com>
2023-10-03 09:18:54 +0100

    first commit

---
+++ frontend/components/UpdateForm.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const UpdateForm = () => {
+  return (
+    <View>
+      <Text>UpdateForm</Text>
+    </View>
+  )
+}
+
+export default UpdateForm
